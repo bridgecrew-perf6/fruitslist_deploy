428f30876f81bc61c645b2b762bd59d8c8526506
hiroyukisss1 <h.i.7.107932@gmail.com>
2022-01-31 16:32:22 +0900

    RDS接続用に変更
    RDS接続用に変更

--- src/main/java/com/example/demo/repository/FruitsMapper.java
+++ src/main/java/com/example/demo/repository/FruitsMapper.java
@@ -2,22 +2,31 @@ package com.example.demo.repository;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.demo.model.User;
 
 @Mapper
 public interface FruitsMapper {
 	
+	@Select("SELECT * FROM fruits WHERE id = #{id}")
     public User findOne(Integer id);
-
+    
+    @Select("SELECT * FROM fruits")
     public List<User> find();
-
+    
+    @Insert("INSERT INTO fruits VALUES(#{id}, #{name}, #{price})")
     public void insertOne(User u);
-
+    
+    @Update("UPDATE fruits SET name = #{name}, price = #{price} WHERE id = #{id}")
     public void updateOne(@Param("id") Integer id,
     		@Param("name") String name, @Param("price") Integer price);
-
+    
+    @Delete("DELETE FROM fruits WHERE id = #{id}")
     public void deleteOne(Integer id);
 }
